f868a591f481551a5214905873a8c5ac7eb1dc8d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-26 12:56:08 +0530

    for construct usage

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -20,4 +20,29 @@ func main() {
 	} else {
 		fmt.Println(no, "is an odd number")
 	}
+
+	//for construct
+	sum := 0
+	for i := 0; i < 10; i++ {
+		sum += i
+	}
+	fmt.Println(sum)
+
+	//imitating the 'while' using 'for'
+	numSum := 1
+	for numSum < 100 {
+		numSum += numSum
+	}
+	fmt.Println(numSum)
+
+	//indefinite loop
+	x := 1
+	for {
+		x += x
+		if x > 100 {
+			break
+		}
+	}
+	fmt.Println(x)
+
 }

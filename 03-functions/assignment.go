6cb5b43b18b502e029e2f40d6791d832c7cc416f
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-26 15:41:14 +0530

    assignment - solution

--- 03-functions/assignment.go
+++ 03-functions/assignment.go
@@ -3,11 +3,40 @@
 	The outcome of increment and decrement should not be able to be influenced by any other code
 */
 
-increment() // => 1
+/* increment() // => 1
 increment() // => 2
 increment() // => 3
 increment() // => 4
 decrement() //=> 3
 decrement() //=> 2
 decrement() //=> 1
-decrement() //=> 0
+decrement() //=> 0 */
+
+package main
+
+import "fmt"
+
+func main() {
+	increment, decrement := getCounters()
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(increment())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+	fmt.Println(decrement())
+}
+
+func getCounters() (func() int, func() int) {
+	count := 0
+	increment := func() int {
+		count++
+		return count
+	}
+	decrement := func() int {
+		count--
+		return count
+	}
+	return increment, decrement
+}

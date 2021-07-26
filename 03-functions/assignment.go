2796001facfd5a4dcc1c82ef0993189dd6b45074
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-26 15:29:27 +0530

    assignment

---
+++ 03-functions/assignment.go
@@ -0,0 +1,13 @@
+/*
+	create increment() and decrement() functions which will increment and decrement the value of the same variable.
+	The outcome of increment and decrement should not be able to be influenced by any other code
+*/
+
+increment() // => 1
+increment() // => 2
+increment() // => 3
+increment() // => 4
+decrement() //=> 3
+decrement() //=> 2
+decrement() //=> 1
+decrement() //=> 0

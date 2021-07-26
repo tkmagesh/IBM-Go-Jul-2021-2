8feffaaec550bf94c8e68f9ff0b192b1d03b770c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-26 12:34:38 +0530

    variable declaration syntax

--- 01-hello-world/program.go
+++ 01-hello-world/program.go
@@ -5,12 +5,93 @@ package main
 import "fmt"
 
 //pakcage level variables & types
+// var appName string = "myapp"
+var appName = "myapp"
 
 //package init function
 
 //main function
 func main() {
-	fmt.Println("Hello World")
+	/*
+		var msg string
+		msg = "Hello World"
+	*/
+	/*
+		var msg string = "Hello World"
+	*/
+	/*
+		var msg = "Hello World"
+	*/
+	//The following syntax is applicable only in a function
+	msg := "Hello World"
+	fmt.Println(msg)
+
+	//multiple variable declarations
+	/*
+		var x int
+		var y int
+		var result int
+		x = 10
+		y = 20
+		result = x + y
+	*/
+	/*
+		var x, y, result int
+		x = 10
+		y = 20
+		result = x + y
+	*/
+	/*
+		var (
+			x      int
+			y      int
+			result int
+			s      string
+		)
+		x = 10
+		y = 20
+		s = "Dummy string"
+		result = x + y
+	*/
+	/*
+		var (
+			x, y, result int
+			s            string
+		)
+		x = 10
+		y = 20
+		s = "Dummy string"
+		result = x + y
+	*/
+	/*
+		var (
+			x, y   int = 10, 20
+			result int
+			s      string = "Dummy string"
+		)
+	*/
+	/*
+		var (
+			x, y   = 10, 20
+			result int
+			s      = "Dummy string"
+		)
+	*/
+	/*
+		x, y := 10, 20
+		result := x + y
+		s := "Dummy string"
+	*/
+	x, y, s := 10, 20, "Dummy string"
+	result := x + y
+
+	fmt.Println(result, s)
+
+	//using package level variable
+	//fmt.Println(appName)
+
+	// var myVar string = "This is a sample string"
+	//myVar := "This is a sample string"
 }
 
 //other functions

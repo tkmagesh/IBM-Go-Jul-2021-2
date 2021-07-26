1009736a24f008cc2573d6cd7dcea127fc42685a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-26 12:09:33 +0530

    data types in golang

---
+++ 01-hello-world/program.go
@@ -0,0 +1,16 @@
+//package declaration
+package main
+
+//import dependent packages
+import "fmt"
+
+//pakcage level variables & types
+
+//package init function
+
+//main function
+func main() {
+	fmt.Println("Hello World")
+}
+
+//other functions

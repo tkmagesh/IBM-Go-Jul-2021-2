f0470872c2ecec6e781cbca92e1643f507ca4bd3
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-27 11:03:06 +0530

    Create assignment.go

---
+++ 05-collections/assignment.go
@@ -0,0 +1,32 @@
+/*
+Given the following string:
+
+Proident occaecat ex esse do duis mollit sunt id ea ut non consectetur incididunt Consectetur culpa non amet ipsum et ex duis Minim reprehenderit est officia est quis laborum laboris aute laboris laboris ut labore Lorem Reprehenderit culpa Lorem non duis nisi mollit cillum eiusmod aliquip est laborum mollit duis Aliqua eu tempor ad aliquip esse nostrud laborum excepteur ipsum excepteur eiusmod Sint nisi duis esse occaecat officia ut amet anim ex veniam est officia labore Elit esse exercitation officia quis ipsum adipisicing fugiat ex irure Culpa dolore laborum amet deserunt aliqua esse duis pariatur cillum est adipisicing Lorem eu Elit anim aute eiusmod consequat exercitation anim laborum consectetur excepteur adipisicing cillum Nostrud proident consequat cupidatat velit fugiat duis Ex tempor eu qui elit nostrud amet Nostrud dolore anim ex aliqua reprehenderit dolor velit adipisicing Consectetur eiusmod incididunt qui proident quis id reprehenderit Sit anim ea eu magna mollit laborum amet voluptate Dolore sunt laboris consectetur tempor nisi ut duis veniam cupidatat do Qui culpa ipsum incididunt sunt velit ipsum Est elit duis aliqua qui dolor aliqua enim laborum reprehenderit magna fugiat consectetur esse Quis nulla fugiat incididunt eu
+
+
+Find out the size of the word that occurs the most by size
+
+ex:
+10 - 2 letters
+7 - 3 letters
+14 - 4 letters
+
+Output:
+	"The size of the word that occurs the most(14 times) is 4"
+
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	str := ""
+	words := strings.Split(str, " ")
+	for idx, word := range words {
+		fmt.Println(idx, word)
+	}
+}

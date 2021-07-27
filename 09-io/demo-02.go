54152ce14d9f369de1d34efd480e33ef473da0a2
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-27 17:44:42 +0530

    Day-02 complete

--- 09-io/demo-02.go
+++ 09-io/demo-02.go
@@ -15,12 +15,32 @@ func (alphaReader AlphaReader) Read(p []byte) (n int, err error) {
 		if (alphaReader[idx] >= 'A' && alphaReader[idx] <= 'Z') || (alphaReader[idx] >= 'a' && alphaReader[idx] <= 'z') {
 			p[count] = alphaReader[idx]
 			count++
+			if count >= len(p) {
+				return count, nil
+			}
 		}
 	}
 	return count, io.EOF
 }
 
+func copy(writer io.Writer, reader io.Reader) {
+	totalBytesRead := 0
+	for {
+		buffer := make([]byte, 10)
+		bytesRead, err := reader.Read(buffer)
+
+		totalBytesRead += bytesRead
+		fmt.Println("Total bytes read ", totalBytesRead)
+		if bytesRead > 0 {
+			writer.Write(buffer[:bytesRead])
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+}
+
 func main() {
 	alphaReader := AlphaReader("this is a sample string with 1234 numbers and *&^%^$ special characters")
-	io.Copy(os.Stdout, alphaReader)
+	copy(os.Stdout, alphaReader)
 }

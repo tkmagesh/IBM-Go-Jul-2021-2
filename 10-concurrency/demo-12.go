d58d0048188639dd47b9dca32b56ce0af533afb4
Magesh Kuppan <tkmagesh77@gmail.com>
2021-07-28 14:38:54 +0530

    divide & conquer assignment

---
+++ 10-concurrency/demo-12.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	/* split the data into two sets, add them concurrently, collate the final result and display the final result */
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+}
